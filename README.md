0296258596fc7f81c6fdbfda0ce2330ca09ca96a
Denfi92 <fuzeev.daniil@yandex.ru>
2022-06-22 17:13:37 +0300

    feat: add test task

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# NewGen-Vision-test
+## [NewGen-Vision-test](https://Denfi92.github.io/NewGen-Vision-test/)
---
+++ index.js
@@ -0,0 +1,28 @@
+let courses = [
+  { name: "Courses in England", prices: [0, 100] }, 
+  { name: "Courses in Germany", prices: [500, null] }, 
+  { name: "Courses in Italy", prices: [100, 200] }, 
+  { name: "Courses in Russia", prices: [null, 400] },
+  { name: "Courses in China", prices: [50, 250] },
+  { name: "Courses in USA", prices: [200, null] },
+  { name: "Courses in Kazakhstan", prices: [56, 324] },
+  { name: "Courses in France", prices: [null, null] },
+];
+
+let requiredRange1 = [null, 200];
+let requiredRange2 = [100, 350];
+let requiredRange3 = [200, null];
+
+function filtered(arrCourses, arrUser){
+  const nullHandlerMin = (arr) => {
+    return (arr[0] === null ? 0 : arr[0])
+  }
+  const nullHandlerMax = (arr) => {
+    return (arr[1] === null ? Infinity : arr[1])
+  }
+  return arrCourses.filter(course => (nullHandlerMin(course.prices) <= nullHandlerMax(arrUser)) && nullHandlerMax(course.prices) >= nullHandlerMin(arrUser));
+}
+
+console.log(filtered(courses, requiredRange1));
+console.log(filtered(courses, requiredRange2));
+console.log(filtered(courses, requiredRange3));
